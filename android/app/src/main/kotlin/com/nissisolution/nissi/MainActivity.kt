27f7c60d9e3e457e25b8d93d7efbcffa2b55698c
NissiSolution <incrediblenissitest@gmail.com>
2024-12-12 15:24:17 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/nissisolution/nissi/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.nissisolution.nissi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/views/dropdown.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class ListViewPage extends StatefulWidget {
+  const ListViewPage({super.key});
+
+  @override
+  State<ListViewPage> createState() => _ListViewPageState();
+}
+
+class _ListViewPageState extends State<ListViewPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("ListView.builder")),
+      body: ListView.builder(
+          itemCount: 5,
+          itemBuilder: (BuildContext context, int index) {
+            return ListTile(
+                leading: const Icon(Icons.list),
+                trailing: const Text(
+                  "GFG",
+                  style: TextStyle(color: Colors.green, fontSize: 15),
+                ),
+                title: Text("List item $index"));
+          }),
+    );
+  }
+}
